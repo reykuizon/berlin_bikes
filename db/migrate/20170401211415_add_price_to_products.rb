454e62bc25cc3de9e2676e89696e65661ddfbed3
Reykuizon <reykuizon@gmail.com>
2017-04-02 10:09:15 +0200

    change _path to _url on nav links

---
+++ db/migrate/20170401211415_add_price_to_products.rb
@@ -0,0 +1,4 @@
+class AddPriceToProducts < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
---
+++ db/migrate/20170401211651_add_prices_to_products.rb
@@ -0,0 +1,5 @@
+class AddPricesToProducts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :price, :decimal
+  end
+end
