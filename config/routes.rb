98ee3d2b8eed5f5f2aba28932487732d51750f04
Reykuizon <reykuizon@gmail.com>
2017-04-03 22:36:54 +0200

    added landing pages and created new contorller ORDER manually

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#index'
+  # root 'static_pages#index'
+  # root 'static_pages#landing_page'
   # root 'products#index'
-  
+  root 'orders#index'
+  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
