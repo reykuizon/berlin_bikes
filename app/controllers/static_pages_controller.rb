66f393657a685d2a2e2dbc64d0269972eee0362f
Reykuizon <reykuizon@gmail.com>
2017-04-07 21:36:48 +0200

    add  Product.limit(3) in static page controller
    added def landing_page and product and display limit to 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  
   def landing_page
     @featured_product = Product.first
   end
+
+  def landing_page
+  @products = Product.limit(3)
+  end
+
 end
