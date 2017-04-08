63dfd11ffca261ea0517fc2095167b92a48b44ec
Reykuizon <reykuizon@gmail.com>
2017-04-08 12:24:34 +0200

    image corousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,17 @@ class StaticPagesController < ApplicationController
   def index
   end
   
-  def landing_page
-    @featured_product = Product.first
-  end
+  # FEATURED PRODUCT, 1ST PRODUCT ON THE DB
+  # def landing_page
+  #   @featured_product = Product.first
+  # end
 
+  # TAKE FIRST 3 PRODUCTS ON DB
   def landing_page
-  @products = Product.limit(3)
+    @products = Product.limit(3)
   end
 
 end
+
+
+
