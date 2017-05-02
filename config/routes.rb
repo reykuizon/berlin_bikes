7d7d30f0a6c06d1c2d30fc33974fc910e9ac3f40
Reykuizon <reykuizon@gmail.com>
2017-05-02 20:16:55 +0200

    added email (contact form)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+
   get 'static_pages/contact'
 
   get 'static_pages/about'
@@ -10,14 +11,15 @@ Rails.application.routes.draw do
 
   get '/products/:id', to: 'products#show'
 
+  post '/static_pages/thank_you'
+
   # root 'static_pages#index'
 
   # root 'static_pages#landing_page'
-  
+
   root 'products#index'
-  
+
   # root 'orders#index'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
