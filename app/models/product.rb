2235b02287ce35018f28cffb346d8bae970624e1
Reykuizon <reykuizon@gmail.com>
2017-05-12 17:34:37 +0200

    Achievement 5.7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
     if Rails.env.development?
@@ -9,4 +10,11 @@ class Product < ApplicationRecord
     end
   end
   
+  def highest_rating_comment
+    comments.rating_desc.pluck(:body).first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.pluck(:body).first
+  end
 end
