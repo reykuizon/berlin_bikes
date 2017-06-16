1f3d7848cd78e7ff0686ed137a040095b331ffa6
Reykuizon <reykuizon@gmail.com>
2017-06-16 16:25:40 +0200

    enable both user_email
    enable both user_emailer and user_mailer_preview

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,10 +6,10 @@ class UserMailer < ApplicationMailer
     mail(:from =>email,
       :to => 'reykuizon83@gmail.com',
       :subject => "A new contact form message from #{name}",
-      :body => @message).deliver
+      
   end
 
-  # WELCOME EMAIL
+  # WELCOME EMAIL TO USER
   def welcome(user)
     @appname = "Berlin Bike Shop"
     mail(to: user.email,
@@ -20,6 +20,7 @@ class UserMailer < ApplicationMailer
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
+  
 end
