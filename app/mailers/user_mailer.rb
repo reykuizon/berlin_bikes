b808d3146c0179a54c31a624b7930112d6b0ec4d
Reykuizon <reykuizon@gmail.com>
2017-08-21 23:13:54 +0200

    edited Stripe mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,30 +1,3 @@
-class UserMailer < ApplicationMailer
-  default from: 'email@berlinbikeshop.com'
-
-#   def contact_form(email, name, message)
-#     @message = message
-#     mail(:from =>email,
-#       :to => 'reykuizon83@gmail.com',
-#       :subject => "A new contact form message from #{name}",
-      
-#   end
-
-  # WELCOME EMAIL TO USER
-  def welcome(user)
-    @appname = "Berlin Bike Shop"
-    mail(to: user.email,
-    subject: "Welcome to #{@appname}!")
-  end
-  
-#   def thank_you
-#     @name = params[:name]
-#     @email = params[:email]
-#     @message = params[:message]
-#     UserMailer.contact_form(@email, @name, @message).deliver_now
-#   end
-  
-
-
 
 class UserMailer < ApplicationMailer
   default from: "berlinbike@example.com"
@@ -36,17 +9,17 @@ class UserMailer < ApplicationMailer
         :subject => "A new contact form message from #{name}")
   end
 
-  def welcome_message(user)
+  def welcome(user)
     @user = user
     mail(:to => user.email,
-         :subject => "Welcome to  Bikes Berlin!")
+         :subject => "Welcome to Berlin Bikes!")
   end
 
-  def successful_payment(user, product)
+  def thanks(user, product)
     @user = user
     @product = product
     mail(:to => user.email,
-        :subject => "Confirmation of payment #{product.name}")
+        :subject => "Thank you for purchasing #{@product.name}").deliver_now
   end
 
 end
